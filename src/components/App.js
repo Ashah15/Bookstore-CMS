ec228f1377761dfb481eef0754b8b8c2a4238b2a
Ashah15 <ashaabdillahi53@gmail.com>
2021-06-12 00:58:06 +0300

    wrap bookslist and booksform in a div selectore by the app.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import BooksList from '../containers/BooksList';
+import BookForm from '../containers/BookForm';
+
+const App =()=>
+      <div>
+        <BookList />
+        <BooksForm />
+      </div>
+
+export default App;
