fe44af6752bf433b146a4940e0eb69346c3af38a
Ashah15 <ashaabdillahi53@gmail.com>
2021-06-25 23:29:37 +0300

    resolved the linter errors in the books.js file

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import { createStore } from 'redux';
 import App from './components/App';
 import rootReducer from './reducers/index';
 
-const store = createStore(rootReducer);
-
 const initialState = [
   { id: 0, title: 'Friday 13', category: 'Horror' },
   { id: 1, title: 'Wild-life Dangers', category: 'Learning' },
   { id: 2, title: 'Toonami', category: 'Kids' },
 ];
+
+const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,9 +22,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-export default initialState;
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,6 +1,4 @@
-import initialState from '../src/initialState';
-
-const bookReducer = (state = initialState, action) => {
+const bookReducer = (state = [], action) => {
   switch (action.type) {
     case 'CREATE_BOOK':
       return [
