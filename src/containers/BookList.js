fbbbbbeef721aca02cc480b0b2850958a248f4d4
Ashah15 <ashaabdillahi53@gmail.com>
2021-07-02 01:29:52 +0300

    add new column to render remove-book

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
+import { removeBook } from '../actions/index';
 
-function BookList({ books }) {
-  const bookList = books.map((book) => (
+function BookList({ books, deleteBook }) {
+  const handleRemoveBook = book => deleteBook(book);
+  const bookList = books.map(book => (
     <Book
+      deleteBook={handleRemoveBook}
       book={book}
       key={book.id}
     />
   ));
+
   return (
-    <table className="Books-list">
+    <table className="Book-list">
       <thead>
         <tr>
           <th>Book ID</th>
-          <th>Book Category</th>
           <th>Book Title</th>
+          <th>Book Category</th>
+          <th>Remove Book</th>
         </tr>
       </thead>
       <tbody>
@@ -32,10 +37,17 @@ BookList.propTypes = {
     category: PropTypes.string,
     id: PropTypes.number,
   })).isRequired,
+  deleteBook: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   books: state.books,
 });
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = dispatch => ({
+  deleteBook: book => {
+    dispatch(removeBook(book));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
