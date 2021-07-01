91e8321b08db51206dab9220a2739348b8157178
Ashah15 <ashaabdillahi53@gmail.com>
2021-07-02 01:21:30 +0300

    made the component stateful

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -15,22 +15,28 @@ class BookForm extends Component {
     this.categoryOptions = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   }
 
-  handleChange = (e) => {
+  handleChange = e => {
     const { booksLength } = this.props;
     this.setState({ id: booksLength });
     this.setState({ [e.target.id]: e.target.value });
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
     const { submitNewBook } = this.props;
     submitNewBook(this.state);
     e.target.reset();
+
+    this.setState({
+      id: null,
+      title: '',
+      category: 'Action',
+    });
   }
 
   render() {
     const { defaultCategory } = this.state;
-    const options = this.categoryOptions.map((category) => (
+    const options = this.categoryOptions.map(category => (
       <option
         value={category}
         key={category}
@@ -64,12 +70,12 @@ class BookForm extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   booksLength: state.books.length,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  submitNewBook: (newBook) => {
+const mapDispatchToProps = dispatch => ({
+  submitNewBook: newBook => {
     dispatch(addBook(newBook));
   },
 });
